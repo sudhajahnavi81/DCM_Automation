dd4058378bbea952936b1bcfaa80bcb3b815c162
sunildesai <sunil.desai@shcr.com>
2019-07-16 21:30:44 +0530

    HTML report

--- DCM/Specflow/Hooks/DCMHooks.cs
+++ DCM/Specflow/Hooks/DCMHooks.cs
@@ -7,6 +7,12 @@ using System.Linq;
 using System.Text;
 using TechTalk.SpecFlow;
 using DCM.POM.Page_Steps;
+using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Gherkin.Model;
+using System.Reflection;
+using BoDi;
+using OpenQA.Selenium.Remote;
 
 namespace DCM.Specflow.Hooks
 {
@@ -16,10 +22,56 @@ namespace DCM.Specflow.Hooks
     public  class Initialize
     {
         public static IWebDriver driver;
+        private static ExtentTest featureName;
+        private static ExtentTest scenario;
+        private static ExtentReports extent;
+        private static KlovReporter klov;
+        private readonly IObjectContainer _objectContainer;
+
+        
+
+        public Initialize(IObjectContainer objectContainer)
+        {
+            _objectContainer = objectContainer;
+        }
+
+
+        [BeforeTestRun]
+        public static void InitializeReport()
+        {
+            //Initialize Extent report before test starts
+            var htmlReporter = new ExtentHtmlReporter(@"D:\DCMAutomation\DCM\ExtentReport.html");
+            htmlReporter.Configuration().Theme = AventStack.ExtentReports.Reporter.Configuration.Theme.Dark;
+            //Attach report to reporter
+            extent = new ExtentReports();
+            //klov = new KlovReporter();
+
+            //klov.InitMongoDbConnection("localhost", 27017);
+
+            //klov.ProjectName = "DCM Automation Test";
+
+            //// URL of the KLOV server
+            //klov.KlovUrl = "http://localhost:5689";
+
+            //klov.ReportName = "DCM Automation" + DateTime.Now.ToString();
+
+
+            extent.AttachReporter(htmlReporter);
+        }
+
+        [AfterTestRun]
+        public static void TearDownReport()
+        {
+            //Flush report once test completes
+            extent.Flush();
+        }
+
 
         [BeforeFeature]
         public static IWebDriver GetDriver()
         {
+            //Create dynamic feature name
+            featureName = extent.CreateTest<Feature>(FeatureContext.Current.FeatureInfo.Title);
 
             if (driver == null)
             {
@@ -45,13 +97,64 @@ namespace DCM.Specflow.Hooks
             
 
         }
-       
+        [AfterStep]
+        public void InsertReportingSteps()
+        {
 
-    [AfterFeature("SanityTest")]
+            var stepType = ScenarioStepContext.Current.StepInfo.StepDefinitionType.ToString();
+
+            PropertyInfo pInfo = typeof(ScenarioContext).GetProperty("TestStatus", BindingFlags.Instance | BindingFlags.NonPublic);
+            MethodInfo getter = pInfo.GetGetMethod(nonPublic: true);
+            object TestResult = getter.Invoke(ScenarioContext.Current, null);
+
+
+            if (ScenarioContext.Current.TestError == null)
+            {
+                if (stepType == "Given")
+                    scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text);
+                else if (stepType == "When")
+                    scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text);
+                else if (stepType == "Then")
+                    scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text);
+                else if (stepType == "And")
+                    scenario.CreateNode<And>(ScenarioStepContext.Current.StepInfo.Text);
+            }
+            else if (ScenarioContext.Current.TestError != null)
+            {
+                if (stepType == "Given")
+                    scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text).Fail(ScenarioContext.Current.TestError.InnerException);
+                else if (stepType == "When")
+                    scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text).Fail(ScenarioContext.Current.TestError.InnerException);
+                else if (stepType == "Then")
+                    scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text).Fail(ScenarioContext.Current.TestError.Message);
+            }
+
+            //Pending Status
+            if (TestResult.ToString() == "StepDefinitionPending")
+            {
+                if (stepType == "Given")
+                    scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text).Skip("Step Definition Pending");
+                else if (stepType == "When")
+                    scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text).Skip("Step Definition Pending");
+                else if (stepType == "Then")
+                    scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text).Skip("Step Definition Pending");
+
+            }
+            extent.Flush();
+        }
+        [BeforeScenario]
+        public void Init()
+        {
+            
+        //Create dynamic scenario name
+        scenario = featureName.CreateNode<Scenario>(ScenarioContext.Current.ScenarioInfo.Title);
+        }
+    [AfterFeature]
         public static void Afterfeature()
         {
             DCM_Login.DCMlogout();
             driver.Close();
+            extent.Flush();
         }
     }
 }
